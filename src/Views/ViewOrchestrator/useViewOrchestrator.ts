b38a7f0d3280d800d9aada27ea6d51adf7b69343
Alex TYRODE <alex.tyrode@outlook.fr>
2024-06-24 00:50:21 +0200

    reinstated quick editor

--- src/Views/ViewOrchestrator/useViewOrchestrator.ts
+++ src/Views/ViewOrchestrator/useViewOrchestrator.ts
@@ -66,7 +66,7 @@ export const useViewOrchestrator = (
       trigger.value = !trigger.value;
     },
     markwhenState: () => {
-      trigger.value = !trigger.value;
+      //trigger.value = !trigger.value;
     }
   });
 
